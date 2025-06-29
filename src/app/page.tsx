af6ae0dde5ef74c36b44384f386a296c0bc31d84
maxckes <ekarshasumajkotikalapoodi@gmail.com>
2025-06-29 14:38:07 +0530

    Add Inngest integration with new API routes, client setup, and UI enhancements including toast notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
+'use client';
 import { Button } from "@/components/ui/button";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import Client from "./client";
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "client" }));
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
+export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: (data) => {
+      console.log(data);
+      toast.success("Event sent");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error("Event failed");
+    },
+  }));
   return (
     <>
-      <div className="font-bold">Hello World</div>
-      <Button>Hello</Button>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+      <div className="text-2xl font-bold p-4 max-w-7xl mx-auto">
+        <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "test" })}>
+          Invoke Inngest
+        </Button>
+      </div>
     </>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,20 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
+    invoke: baseProcedure
+    .input(z.object({
+        text: z.string(),
+    }))
+    .mutation(async ({input}) => {
+        await inngest.send({
+            name: "test/hello.world",
+            data: {
+                text: input.text,
+            },
+        });
+        return { message: "Event sent",ok:"success" };
+    }),
   hello: baseProcedure
     .input(
       z.object({
