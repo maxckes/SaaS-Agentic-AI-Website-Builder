9465b9dde148d630904f6452768fd0d6de78350e
maxckes <ekarshasumajkotikalapoodi@gmail.com>
2025-06-29 15:35:21 +0530

    Add Inngest agent integration and UI enhancements
    - Introduced @inngest/agent-kit dependency in package.json and package-lock.json.
    - Updated the Home component to include an Input field for user input.
    - Modified the invoke mutation to send user input value instead of a static text.
    - Enhanced the helloWorld function to utilize the Inngest agent for processing input and returning output.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
   const trpc = useTRPC();
+  const [value, setValue] = useState("");
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: (data) => {
       console.log(data);
@@ -18,8 +21,9 @@ export default function Home() {
   }));
   return (
     <>
-      <div className="text-2xl font-bold p-4 max-w-7xl mx-auto">
-        <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "test" })}>
+      <div className=" p-4 max-w-7xl mx-auto">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value:value })}>
           Invoke Inngest
         </Button>
       </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,17 @@
 import { inngest } from "./client";
-
+import { createAgent,gemini } from "@inngest/agent-kit";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    await step.sleep("wait-a-moment", "10s");
-    await step.sleep("wait-a-moment", "10s");
-
-    return { message: `Hello ${event.data.email}!` };
+    await step.sleep("wait-a-moment", "3s");
+     const summaryAgent = createAgent({
+        name: "summary-agent",
+        system: "You are an expert react js and next js developer.  You write readable, concise, maintainable code. you write code in a way that is easy to understand and easy to maintain. write snippet of code that is easy to understand and easy to maintain.",
+        model: gemini({ model: "gemini-2.0-flash" }),
+      });
+      const {output} = await summaryAgent.run(`write following snippet: ${event.data.value}`);
+      console.log(output);
+    return { output };
   },
 );
