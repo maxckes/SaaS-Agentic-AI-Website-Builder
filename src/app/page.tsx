8ea023a46fa9fe25086d9a99f550a9db110780ba
maxckes <ekarshasumajkotikalapoodi@gmail.com>
2025-06-29 11:28:15 +0530

    Add Prisma setup, new UI components, and mobile responsiveness features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
     <>
-    <div className="font-bold">Hello World</div>
-    <Button>Hello</Button>
+      <div className="font-bold">Hello World</div>
+      <Button>Hello</Button>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </>
   );
 }
