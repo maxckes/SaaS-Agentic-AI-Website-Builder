4aadbbfa0167cf06e1b95dc136cf68ab9bc4d4dc
maxckes <ekarshasumajkotikalapoodi@gmail.com>
2025-07-01 22:17:28 +0530

    Add E2B sandbox integration and update dependencies
    - Added @e2b/code-interpreter dependency in package.json and package-lock.json.
    - Introduced new sandbox templates for Next.js, including Dockerfile and compile script.
    - Enhanced helloWorld function to create and retrieve sandbox URL using E2B integration.
    - Added utility function to connect to the sandbox and retrieve its URL.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,27 @@
 import { inngest } from "./client";
 import { createAgent,gemini } from "@inngest/agent-kit";
+import {Sandbox} from "@e2b/code-interpreter";
+import { getSandboxUrl } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "3s");
+    const sandboxId = await step.run("get-sandbox-id",async()=>{
+      const sandbox = await Sandbox.create('ai-saas-website-builderx')
+      return sandbox.sandboxId
+    })
      const summaryAgent = createAgent({
         name: "summary-agent",
         system: "You are an expert react js and next js developer.  You write readable, concise, maintainable code. you write code in a way that is easy to understand and easy to maintain. write snippet of code that is easy to understand and easy to maintain.",
         model: gemini({ model: "gemini-2.0-flash" }),
       });
       const {output} = await summaryAgent.run(`write following snippet: ${event.data.value}`);
-      console.log(output);
-    return { output };
+      const sandboxUrl = await step.run("get-sandbox-url",async()=>{
+        const sandbox = await getSandboxUrl(sandboxId)
+        const host =  sandbox.getHost(3000)
+        return `https://${host}`
+      })
+    return { output,sandboxUrl };
   },
 );
